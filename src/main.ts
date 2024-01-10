06240c0029edd00890e26130e9af5ff044eb1589
mariamoratalla <maria.moratalla97@gmail.com>
2024-01-10 12:29:48 +0100

    calculo hecho

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,8 @@
 import "./style.css";
 
-console.log("Hello Typescript!");
+//Considerando que el grupo son 5 amigos y yo
+
+console.log(
+  "Mis amigos tendrían que pagar " + (120 - 18) / 6 + " euros cada uno"
+);
+console.log("Yo tendría que pagar " + ((120 - 18) / 6 + 18) + " euros");
